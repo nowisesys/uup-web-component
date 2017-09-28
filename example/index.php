8bbb8aae5640d52516eb46a6293e6e7e1ba31c74
Anders Lövgren <andlov@nowise.se>
2017-09-28 22:57:08 +0000

    Fixing up examples. Menu on left hand side using flexbox for splitting in two columns.

--- example/index.php
+++ example/index.php
@@ -1,27 +1,25 @@
-<?php
-/*
- * Copyright (C) 2017 Anders Lövgren (QNET).
- *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- */
+<link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
 
-require_once("../vendor/autoload.php");
+<div style="padding: 10px">
+    <h1>UUP Web Components</h1>
+    <p>
+        Components should be rendered for W3.CSS style sheet. Some test might apply 
+        transformation of CSS-classes in components that makes them look weird.
+    </p>
 
-?>
+    <?php
+    require_once("../vendor/autoload.php");
+    require_once("index.inc");
 
-<h1>Index page for UUP Web Components</h1>
-<ul>
-    <li><a href="elements.php">Element components</a></li>
-    <li><a href="collection.php">Collection classes</a></li>
-    <li><a href="regexp.php">Regexp pattern matching</a></li>
-</ul>
+    echo "<div style=\"display: flex\">\n";
+    echo "<div>\n";
+    print_menu(include('menu.inc'));
+    echo "</div>\n";
+    echo "<div style=\"width: 100%\">\n";
+    load_example();
+    echo "</div>\n";
+    echo "</div>\n";
+
+    ?>
+
+</div>
