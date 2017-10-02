476e01c506aa71282a4fd841d0b20b571a3d8af4
Anders Lövgren <andlov@nowise.se>
2017-10-02 15:59:45 +0000

    Adding virtual color and hover property collections.

---
+++ source/UUP/Web/Component/Collection/Properties/Virtual.php
@@ -0,0 +1,68 @@
+<?php
+
+/*
+ * Copyright (C) 2017 Anders Lövgren (Computing Department at BMC, Uppsala University).
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+namespace UUP\Web\Component\Collection\Properties;
+
+use DomainException;
+use UUP\Web\Component\Collection\Properties;
+
+/**
+ * Virtual properties collection.
+ * 
+ * @author Anders Lövgren (Computing Department at BMC, Uppsala University)
+ */
+class Virtual
+{
+
+        /**
+         * The prefix string (i.e. color).
+         * @var string 
+         */
+        private $_prefix;
+        /**
+         * The shadowed properties collection.
+         * @var Properties 
+         */
+        private $_props;
+
+        /**
+         * Constructor.
+         * @param string $prefix The prefix string (i.e. color).
+         * @param Properties $props The shadowed properties collection.
+         */
+        public function __construct($prefix, $props)
+        {
+                $this->_prefix = $prefix;
+                $this->_props = $props;
+        }
+
+        public function __get($name)
+        {
+                return $this->_props->get(sprintf("%s-%s", $this->_prefix, $name));
+        }
+
+        public function __set($name, $value)
+        {
+                if (!is_string($value)) {
+                        throw new DomainException("Expected string value");
+                } else {
+                        $this->_props->set(sprintf("%s-%s", $this->_prefix, $name), $value);
+                }
+        }
+
+}
